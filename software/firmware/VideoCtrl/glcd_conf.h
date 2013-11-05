b975034732dcd41f26031579b1e8d8d3174d2875
Peter Schuster <schuster.pe@gmail.com>
2013-11-05 13:38:02 +0100

    glcd integration.

---
+++ software/firmware/VideoCtrl/glcd_conf.h
@@ -0,0 +1,15 @@
+/*
+ * glcd_conf.h
+ *
+ *  Created on: 31.10.2013
+ *      Author: Peter Schuster
+ */
+
+#ifndef GLCD_CONF_H_
+#define GLCD_CONF_H_
+
+#define GLCD_DEVICE_CHIBIOS 		TRUE
+#define GLCD_CONTROLLER_ST7565R		TRUE
+#define GLCD_USE_SPI				TRUE
+
+#endif /* GLCD_CONF_H_ */
