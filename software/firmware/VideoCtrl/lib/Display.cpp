30f90c746b1ab1c93798cf93a556e3c9bf366aea
Peter Schuster <schuster.pe@gmail.com>
2013-11-13 19:30:32 +0100

    Implemented display buttons.

--- software/firmware/VideoCtrl/lib/Display.cpp
+++ software/firmware/VideoCtrl/lib/Display.cpp
@@ -7,6 +7,7 @@
 
 #include "ch.h"
 #include <string.h>
+
 extern "C" {
 #include "glcd.h"
 #include "glcd_graphs.h"
@@ -16,6 +17,7 @@ extern "C" {
 #include "font5x7.h"
 #include "Bebas_Neue20x36_Bold_Numbers.h"
 }
+
 #include "Display.h"
 #include "chthreads.h"
 #include <string.h>
@@ -24,6 +26,11 @@ Display::Display(SPIDriver *spip, I2cBus *i2cBus) {
 	_eaDogL = new EaDogL(spip, GPIOD, GPIOD_PIN0);
 	_buttons = new MCP23017();
 	_buttons->begin(i2cBus, 0);
+
+	_buttonStatus = 0;
+	_buttonStatusLastDown = 0;
+	_buttonStatusLastUp = 0;
+	_enc1 = _enc2 = 0;
 }
 
 void Display::init() {
@@ -64,4 +71,79 @@ void Display::init() {
 	glcd_draw_string_xy(20,40,(char*)"VideoController");
 
 	glcd_write();
+}
+
+bool Display::getButtonLed(int buttonNumber) {
+
+    if (!_validButtonNumber(buttonNumber))
+        return false;
+
+    uint16_t bits;
+    switch (buttonNumber) {
+        case 1:
+            bits = (_dataReg >> DISP_B1_LED_BIT) & 0b1;
+            break;
+        case 2:
+            bits = (_dataReg >> DISP_B2_LED_BIT) & 0b1;
+            break;
+        case 3:
+            bits = (_dataReg >> DISP_B3_LED_BIT) & 0b1;
+            break;
+        case 4:
+            bits = (_dataReg >> DISP_B4_LED_BIT) & 0b1;
+            break;
+        default:
+            bits = 1;
+            break;
+    }
+
+    return (bits == 0);
+}
+
+void Display::setButtonLed(int buttonNumber, bool on) {
+
+    if (!_validButtonNumber(buttonNumber))
+        return;
+
+    uint16_t bits;
+
+    switch (buttonNumber) {
+        case 1:
+            bits = 1 << DISP_B1_LED_BIT;
+            break;
+        case 2:
+            bits = 1 << DISP_B2_LED_BIT;
+            break;
+        case 3:
+            bits = 1 << DISP_B3_LED_BIT;
+            break;
+        case 4:
+            bits = 1 << DISP_B4_LED_BIT;
+            break;
+        default:
+            bits = 0;
+            break;
+    }
+
+    // inverted logic (switched gnd)
+    _dataReg = on ? _dataReg & ~bits : _dataReg | bits;
+
+    _buttons->digitalWordWrite(_dataReg);
+}
+
+void Display::readButtonStatus() {	// Reads button status from MCP23017 chip.
+    uint16_t dataReg = _buttons->digitalWordRead();
+
+    // Read buttons status bits
+	_buttonStatus =
+		(uint8_t)(((dataReg & (1 << DISP_B1_BIT)) >> DISP_B1_BIT))      |   // B1
+        (uint8_t)(((dataReg & (1 << DISP_B2_BIT)) >> DISP_B2_BIT) << 1) |   // B2
+        (uint8_t)(((dataReg & (1 << DISP_B3_BIT)) >> DISP_B3_BIT) << 2) |   // B3
+        (uint8_t)(((dataReg & (1 << DISP_B4_BIT)) >> DISP_B4_BIT) << 3);    // B4
+
+	_dataReg = dataReg;
+}
+
+bool Display::_validButtonNumber(int buttonNumber) {
+    return (buttonNumber>=1 && buttonNumber <= 4);
 }
--- software/firmware/VideoCtrl/lib/Display.h
+++ software/firmware/VideoCtrl/lib/Display.h
@@ -13,14 +13,40 @@
 #include "hw/EaDogL.h"
 #include "I2cBus.h"
 #include "hw/MCP23017.h"
+#include "Buttons.h"
 
-class Display {
+#define DISP_B1_BIT       3
+#define DISP_B2_BIT       1
+#define DISP_B3_BIT       0
+#define DISP_B4_BIT       2
+#define DISP_B1_LED_BIT   7
+#define DISP_B2_LED_BIT   5
+#define DISP_B3_LED_BIT   4
+#define DISP_B4_LED_BIT   6
+#define DISP_ENC1_A_BIT  11
+#define DISP_ENC1_B_BIT  12
+#define DISP_ENC2_A_BIT  14
+#define DISP_ENC2_B_BIT  15
+
+class Display : public Buttons {
 private:
 	EaDogL* _eaDogL;
 	MCP23017* _buttons;
+
+	uint16_t    _dataReg;
+	int8_t      _enc1, _enc2;
+
 public:
 	Display(SPIDriver* spip, I2cBus* i2cBus);
 	void init();
+
+	bool getButtonLed(int buttonNumber);
+	void setButtonLed(int buttonNumber, bool on);
+
+	virtual void readButtonStatus();
+
+protected:
+  virtual bool _validButtonNumber(int buttonNumber);
 };
 
 #endif /* DISPLAY_H_ */
