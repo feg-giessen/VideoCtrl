aecab35a7dc85dc0fc3bb257fbb5d48bf0a2a193
Peter Schuster <schuster.pe@gmail.com>
2014-01-30 21:25:33 +0100

    Set initial button mapping on migration

--- software/firmware/VideoCtrl/app/Memory.cpp
+++ software/firmware/VideoCtrl/app/Memory.cpp
@@ -7,6 +7,7 @@
 
 #include "Memory.h"
 #include "lwip/ip_addr.h"
+#include "Videomischer.h"
 
 Memory::Memory() {
 	_ip_address = 0;
@@ -28,6 +29,27 @@ void Memory::migrate() {
 		IP4_ADDR(&ip, 192, 168, 40, 20);
 
 		setIpAddress(ip.addr);
+
+		button_mapping_t default_mapping;
+		default_mapping.b1  = (uint8_t)ATEM_Program;
+		default_mapping.b2  = (uint8_t)ATEM_Preview;
+		default_mapping.b3  = (uint8_t)ATEM_Aux1;
+		default_mapping.b4  = (uint8_t)ATEM_None;
+		default_mapping.b5  = (uint8_t)ATEM_None;
+		default_mapping.b6  = (uint8_t)ATEM_Dsk1;
+		default_mapping.b7  = (uint8_t)ATEM_Macro1;
+		default_mapping.b8  = (uint8_t)ATEM_FadeToBlack;
+
+		default_mapping.b9  = (uint8_t)ATEM_In1;
+		default_mapping.b10 = (uint8_t)ATEM_In2;
+		default_mapping.b11 = (uint8_t)ATEM_In3;
+		default_mapping.b12 = (uint8_t)ATEM_In4;
+		default_mapping.b13 = (uint8_t)ATEM_In5;
+		default_mapping.b14 = (uint8_t)ATEM_In6;
+		default_mapping.b15 = (uint8_t)ATEM_None;
+		default_mapping.b16 = (uint8_t)ATEM_InBlack;
+
+		setButtonMapping(0, &default_mapping);
 	}
 }
 
